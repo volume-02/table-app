b634c602fb8c3980f2924e689a2a09a60b26c690
Boris <borsukboris@gmail.com>
2020-02-24 22:02:27 +0300

    offset + pagination

--- src/components/routes/table/Table.tsx
+++ src/components/routes/table/Table.tsx
@@ -6,6 +6,11 @@ interface Props {}
 
 export const Table: React.FC<Props> = () => {
   const [data, setData] = useState<any>({});
+  const [start, setStart] = useState(0);
+  // const [stop, setStop] = useState(9);
+  const [offset, setOffset] = useState(10);
+
+  let stop = start + offset - 1;
 
   const fetchData = async () => {
     const { data } = await getData();
@@ -18,15 +23,50 @@ export const Table: React.FC<Props> = () => {
 
   console.table('table ', data.results);
 
+  const changePage = (next: boolean = true) => {
+    if (next) {
+      setStart(start + offset);
+    } else {
+      setStart(start - offset);
+    }
+  };
+
+  console.log('start, stop', start, stop);
+
   if (!data.results) {
     return <h1>Loading...</h1>;
   }
 
   if (data.results) {
+    const paginatedData = data.results.filter(
+      (item: object, index: number) => index >= start && index <= stop
+    );
+
+    console.log('paginatedData', paginatedData);
+
     return (
       <div>
         <h4>{data.results.length} results</h4>
-        <TableRow data={data.results} />
+        <TableRow data={paginatedData} />
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            width: 1200
+          }}
+        >
+          <button onClick={() => changePage(false)}>туда</button>
+          <select
+            name="select"
+            defaultValue="10"
+            onChange={e => setOffset(+e.target.value)}
+          >
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+          </select>
+          <button onClick={() => changePage()}>сюда</button>
+        </div>
       </div>
     );
   }
